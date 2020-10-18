6c0b30b5e91cde9679d712c62032829cf9a9ceca
jorgsor <73069107+jorgsor@users.noreply.github.com>
2020-10-18 17:23:10 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'jorgen.sorheim');
+define('DB_SERVER', 'forevery.database.windows.net');
+define('DB_USERNAME', 'jorgen.sorheim@southbrook.capital');
 define('DB_PASSWORD', 'Jf89j9825q!');
-define('DB_NAME', 'forevery');
+define('DB_NAME', 'persons');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
